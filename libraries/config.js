28191217b34291b67234f319ea8118be7fbcfdad
javohirfrontenddevs <javohirabdusharipov676@gmail.com>
2025-02-24 23:03:01 +0500

    finihed the project

--- libraries/config.js
+++ libraries/config.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 
 const mongoDB = async () => {
   try {
-    const connecter = await mongoose.connect(process.env.MONGOURI);
+    const connecter = await mongoose.connect(
+      "mongodb+srv://javohirabdusharipov676:iW6qTLpz39kwoxCV@cluster0.6apzw.mongodb.net/"
+    );
     if (connecter) {
       console.log("Connected");
     } else {
